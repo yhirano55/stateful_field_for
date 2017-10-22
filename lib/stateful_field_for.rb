cad7d5ef721afb2508b5ad217ae97313915ae424
Yoshiyuki Hirano <yhirano@me.com>
2017-10-22 13:00:15 +0900

    Impl StatefulFieldFor

--- lib/stateful_field_for.rb
+++ lib/stateful_field_for.rb
@@ -1,5 +1,13 @@
 require "stateful_field_for/version"
 
 module StatefulFieldFor
-  # Your code goes here...
+  def stateful_field_for(attr_name, options = {}, &block)
+    definition = Definition.new(attr_name, options)
+    definition.instance_eval(&block)
+    const_set definition.module_name, definition._module
+    include definition._module
+  end
 end
+
+require "stateful_field_for/definition"
+require "stateful_field_for/railtie" if defined?(Rails)
